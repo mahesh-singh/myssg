7f99a355fb37b0f0352ec524ecbb3c4e87d929b6
Mahesh Singh <singh.mahesh@gmail.com>
2025-02-17 14:19:05 +0530

    added basic markdown to html conversion based on template

--- cmd/ssg/main.go
+++ cmd/ssg/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"strings"
+
+	"github.com/yuin/goldmark"
+)
+
+type post struct {
+	Title   string
+	Date    string
+	Tags    []string
+	Content template.HTML
+	Summary string
+	Slug    string
+}
 
 func main() {
 	fmt.Print("this is my static site generator")
+	p := post{
+		Title:   "this is a title",
+		Content: ConvertMarkdownToHTML("this is content \n **this is string**"),
+	}
+
+	tmpl, err := template.ParseFiles("templates/base.html")
+	if err != nil {
+		fmt.Printf("error while parsing the template: %v \n", err)
+		return
+	}
+	var htmlContent strings.Builder
+	err = tmpl.Execute(&htmlContent, p)
+
+	if err != nil {
+		fmt.Printf("error while executing the template: %v \n", err)
+		return
+	}
+
+	fmt.Println((htmlContent.String()))
+}
+
+func ConvertMarkdownToHTML(md string) template.HTML {
+	var buf bytes.Buffer
+
+	if err := goldmark.Convert([]byte(md), &buf); err != nil {
+		fmt.Printf("error while converting markdown into into html: %v \n", err)
+		return ""
+	}
+	return template.HTML(buf.String())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mahesh-singh/myssg
 
 go 1.23.0
+
+require github.com/yuin/goldmark v1.7.8 // indirect
